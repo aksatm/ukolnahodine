c77d15e5876e676ec7d5dec2b9ae7c22a68cc0fa
aksatm <mehmedemin.aksat@sosasou.cz>
2025-12-16 08:53:40 +0100

    rodic
    cely rodic vypracovany

--- ukol3/ukol3/rodic.cpp
+++ ukol3/ukol3/rodic.cpp
@@ -1,1 +1,30 @@
 #include "rodic.h"
+#include <iostream>
+
+Rodic::Rodic(const std::string& name, int age, int weight)
+    : mName(name), mAge(age), mWeight(weight)
+{
+    std::cout << "Rodicovy constructor called for " << mName << '\n';
+}
+
+Rodic::~Rodic() = default;
+
+const std::string& Rodic::getName() const
+{
+    return mName;
+}
+
+int Rodic::getAge() const
+{
+    return mAge;
+}
+
+int Rodic::getWeight() const
+{
+    return mWeight;
+}
+
+void Rodic::speak() const
+{
+    std::cout << getName() << " rika ahooooooj.\n";
+}
--- ukol3/ukol3/rodic.h
+++ ukol3/ukol3/rodic.h
@@ -1,5 +1,28 @@
 #pragma once
-class rodic
+#include <string>
+
+class Rodic
 {
+public:
+    // Konstruktor zakladni tridy, ma stejny nazev jako trida
+    Rodic(const std::string& name,
+        int age,
+        int weight);
+    // Virtualni destruktor pro spravne uvolneni pameti pri dedeni
+    // destruktor ma stejny nazev jako trida, ale s tridou (~) pred ni
+    virtual ~Rodic();
+
+    //dalsi metody
+    const std::string& getName() const;
+    int getAge() const;
+    int getWeight() const;
+
+    // Virtualni metoda, aby odvozene tridy mohly popsat chovani
+    virtual void speak() const;
+
+protected:
+    std::string mName;
+    int mAge;
+    int mWeight;
 };
 
